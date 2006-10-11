019ca6a8105b2656dabbc09a5922cf0b4acf54dd
mchristen <mchristen@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2006-10-11 13:58:22 +0000

    a test index.php

---
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/ClockworkCoders/index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+
+<title>Hello OpenGL</title>
+
+<?php include("http://www.opengl.org/inc/sdk_head.txt"); ?>
+
+</head>
+
+<body>
+
+<?php include("http://www.opengl.org/inc/sdk_body_start.txt"); ?>
+
+<h1>Tutorial: OpenGL Hello World</h1>
+
+<pre>std::cout << "hello OpenGL\n";</pre>
+
+
+<?php include("http://www.opengl.org/inc/sdk_footer.txt"); ?>
+
+</body>
+
+</html>

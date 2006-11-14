7819a4dd058f26c34e3681ce6562ef1241616708
philippcrocoll <philippcrocoll@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2006-11-14 16:24:37 +0000

    + simple index file

---
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/CodeColony/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>CodeColony.de SDK contribution</title>
+<?php include("http://www.opengl.org/inc/sdk_head.txt"); ?>
+</head>
+
+<body>
+<?php include("http://www.opengl.org/inc/sdk_body_start.txt"); ?>
+<h1>Simple Index</h1>
+
+<a href="camera.php">Camera</a><br>
+<a href="fountain.php">Fountain</a><br>
+<a href="vertexarrays.php">Vertex Arrays</a><br>
+<br>
+
+<a href="http://www.codecolony.de">www.codecolony.de</a>
+
+<?php include("http://www.opengl.org/inc/sdk_footer.txt"); ?>
+</body>
+
+</html>

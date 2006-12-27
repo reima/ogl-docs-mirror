56080dd5e8e576a742f04be1eb0e2e5fb7318f5d
mchristen <mchristen@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2006-12-27 09:34:11 +0000

    layout test 3

--- ogl/trunk/ecosystem/public/sdk/docs/tutorials/ClockworkCoders/test.php
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/ClockworkCoders/test.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/tutorial.dwt" codeOutsideHTMLIsLocked="false" -->
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Untitled Document</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
-<p>
-   <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
-</p>
-</head>
+<html>
 
+<head>
+<title>Tutorials</title>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
+</head>
 <body>
 
 <!-- InstanceBeginEditable name="Title" -->

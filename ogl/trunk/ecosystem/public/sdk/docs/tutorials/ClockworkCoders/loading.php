7854054b16f0b381353692c5fff79de9f43fcde5
mchristen <mchristen@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2006-12-29 09:44:46 +0000

    updated GLSL-Texturing tutorial

--- ogl/trunk/ecosystem/public/sdk/docs/tutorials/ClockworkCoders/loading.php
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/ClockworkCoders/loading.php
@@ -55,11 +55,13 @@
     </table>
     <h2>Loading Shader</h2>
     <p>Now we have two simple shaders, a vertex and a fragment shader. If the shaders are located in text files they must be loaded into memory first. <span style="font-weight: bold">This part has nothing to do with OpenGL</span>, it is a simple ASCII file loader. If you write your shaders in Unicode (for the comments), you have to write your own loader. The actual program in memory should be in ASCII. You could also embed your shaders into your C++ code using a static char array. In other words: it doesn't matter how you get your shaders into memory. I recommend using ASCII files. This way you can change your shader code without recompiling your application. The source here simply loads an ASCII Shader File: </p>
-    <table width="90%" border="0" align="center" bordercolor="#0000CC" bgcolor="#EEEEEE">
+    <table width="67%" border="0" align="center" bordercolor="#0000CC" bgcolor="#EEEEEE">
       <tr>
         <td><pre>unsigned long getFileLength(ifstream&amp; file)<br>{<br>    if(!file.good()) return 0;<br>    <br>    unsigned long pos=file.tellg();<br>    file.seekg(0,ios::end);<br>    unsigned long len = file.tellg();<br>    file.seekg(ios::beg);<br>    <br>    return len;<br>}
 
-int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len)<br>{<br>   ifstream file;<br>	file.open(filename, ios::in);<br>   if(!file) return -1;<br>    <br>   len = getFileLength(file);<br>    <br>   if (len==0) return -2;   // Error: Empty File <br>    <br>   *ShaderSource = (GLubyte*) new char[len+1];<br>   if (*ShaderSource == 0) return -3;   // can't reserve memory<br>     <br>   *ShaderSource[len] = 0;  // len isn't always strlen cause some characters are stripped in ascii read...<br>                           // it is important to 0-terminate the real length later, len is just max possible value...<br>   unsigned int i=0;<br>   while (file.good())<br>   {<br>       *ShaderSource[i] = file.get();       // get character from file.<br>       if (!file.eof())<br>        i++;<br>   }<br>    <br>   *ShaderSource[i] = 0;  // 0 terminate it.<br>    <br>   file.close();<br>      <br>   return 0; // No Error<br>}
+int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len)<br>{<br>   ifstream file;<br>   file.open(filename, ios::in); // opens as ASCII!<br>   if(!file) return -1;<br>    <br>   len = getFileLength(file);<br>    <br>   if (len==0) return -2;   // Error: Empty File <br>    <br>   *ShaderSource = (GLubyte*) new char[len+1];<br>   if (*ShaderSource == 0) return -3;   // can't reserve memory<br>   
+    // len isn't always strlen cause some characters are stripped in ascii read...
+    // it is important to 0-terminate the real length later, len is just max possible value... <br>   *ShaderSource[len] = 0; <br><br>   unsigned int i=0;<br>   while (file.good())<br>   {<br>       *ShaderSource[i] = file.get();       // get character from file.<br>       if (!file.eof())<br>        i++;<br>   }<br>    <br>   *ShaderSource[i] = 0;  // 0-terminate it at the correct position<br>    <br>   file.close();<br>      <br>   return 0; // No Error<br>}
 
 
 int unloadshader(GLubyte** ShaderSource)
@@ -178,8 +180,9 @@ if (linked)
     It is also possible to get a linker log, similar to the compiler log. Instead of using the ShaderObject, use the ProgramObject. </p>
     <h2>Using Shaders</h2>
     <p>Once you have a linked Program Object, it is very easy to use that shader with the OpenGL function <a href="http://www.opengl.org/sdk/docs/man/xhtml/glUseProgram.xml" target="_blank">glUseProgram</a> with the program object as argument. To stop using the program you can call glUseProgram(0).</p>
-    <p>As you see it is pretty easy to load, compile, link and use shaders, on the other side it is pretty complicated if you use many different shaders in your code. I created some C++ classes which simplify the whole process.</p>
-    <p>the class cwc::glShaderManager loads, compiles and links a GLSL program (from file or memory). It returns a cwc::glShader, which holds/represents the &quot;Program Object&quot;.</p>
+    <h2>Example Project </h2>
+    <p>As you see it is pretty easy to load, compile, link and use shaders. On the other side it is pretty complicated if you use many different shaders in your code. I created some C++ classes which simplify the whole process.</p>
+    <p>the class cwc::glShaderManager loads, compiles and links a GLSL program (from file or memory). It returns a cwc::glShader, which holds/represents the &quot;Program Object&quot;. There are many other useful things in that class. It is the &quot;libglsl&quot; I created a while back. </p>
     <table width="90%" border="0" align="center" bordercolor="#0000CC" bgcolor="#EEEEEE">
       <tr>
         <td><pre>#include &quot;glsl.h&quot;<br>

20d4dcec034a3639a21d2129f6d91032888bb033
mchristen <mchristen@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-01-03 10:41:09 +0000

    Added some images to "discard" tutorial.

--- tutorials/ClockworkCoders/discard.php
+++ tutorials/ClockworkCoders/discard.php
@@ -21,21 +21,51 @@
     <!-- InstanceEndEditable --><!-- InstanceBeginEditable name="Tutorial Text" -->
     <p>It is possible to tell the fragment shader it shouldn't write any pixel. This can be done using the &quot;discard&quot; statement.</p>
     <p>This can be used - for example - to implement a color key. </p>
+    <p>Using the following texture, all red pixels should be removed (discarded!). </p>
+    <p align="center"><img src="texture.gif" width="256" height="256"> <br>
+    Texture with with red color key </p>
+    <p>&nbsp;</p>
     <table width="90%" border="0" align="center" bordercolor="#0000CC" bgcolor="#EEEEEE">
       <tr>
-        <td><pre>varying vec2 vTexCoord;
-
-
-void main (void)  <br>{     
-   if (pixel in texture == key_value)
-     discard;
-<br>   gl_FragColor = vec4(0.0,0.0,1.0,1.0);  <br>}</pre></td>
+        <td><pre>varying vec2 vTexCoord;</pre>
+          <pre>void main(void)
+{
+   vTexCoord = gl_MultiTexCoord0.xy;
+   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}        </pre>          
+          </td>
+      </tr>
+      <tr>
+        <td bgcolor="#CCCCFF">Vertex Shader Source Code </td>
+      </tr>
+    </table>
+    <br>
+    <table width="90%" border="0" align="center" bordercolor="#0000CC" bgcolor="#EEEEEE">
+      <tr>
+        <td><pre>sampler2D myTexture;
+           varying vec2 vTexCoord;</pre>
+          <pre>void main (void) 
+{  
+   vec4 color = texture2D(myTexture, vTexCoord); 
+             
+   if (color.rgb == vec3(1.0,0.0,0.0))
+      discard; 
+   
+   gl_FragColor = color;
+} </pre>          <pre>&nbsp;</pre></td>
       </tr>
       <tr>
         <td bgcolor="#CCCCFF"><span style="font-style: italic">Fragment Shader Source Code </span></td>
       </tr>
     </table>
     <p>&nbsp;</p>
+    <p>If you start the shader you will get the following result:</p>
+    <p align="center"><img src="color-key-bad.gif" width="648" height="510"> </p>
+    <p align="left">There are still some red pixels visible. This is because the texture was filtered. This can be prevented by setting the texture parameters to: </p>
+    <p>glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);<br>
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);</p>
+    <p align="center"><img src="color-key.gif" width="648" height="510"></p>
+    <p align="left">This time it looks better. </p>
     <p>&nbsp;</p>
     <!-- InstanceEndEditable -->
     <table width="100%" border="0">

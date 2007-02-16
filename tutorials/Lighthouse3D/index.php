b8c4bdb246818cff2c1eb4b060ec065b6dd879b8
lighthouse3d <lighthouse3d@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-02-16 18:41:45 +0000

    First attempt at adding some content

---
+++ tutorials/Lighthouse3D/index.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+<title>Lighthouse3D SDK contribution</title>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
+</head>
+
+<body>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
+<h1>Index</h1>
+<P>
+<H2>Tutorials available from the SDK</H2>
+
+<H2>Tutorials available from Lighthouse3D.com</H2>
+<TABLE>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/ledshader/"><font color=#0000FF size=2><B>Led Shader tutorial</B></font></A><BR>
+A great tutorial by Jason Gorski on how to achive the pixelated effect commonly seen in led displays. The tutorial explores in depth how to achieve each of the features of these low res displays. Full source code is made available.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/viewfrustum/"><font color=#0000FF size=2><B>View Frustum Culling tutorial</B></font></A><BR>
+View frustum culling is a simple way to add a boost to an OpenGL application. This tutorial shows how to extract the view frustum for a camera, and how to cull spheres and boxes that are outside the view frustum. Optimizations, notes, references and source code are also provided.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/glsl/"><font color=#0000FF size=2><B>GLSL tutorial</B></font></A><BR>
+
+An introduction to GLSL that not only covers shader writing, but also covers the graphics pipeline, the OpenGL Setup of an application using shaders.Variables, data types, and language syntax are also explored. Shader examples, namely for toon shading, lighting and texturing, are provided and source code is downloadable. Both ARB extensions and OpenGL 2.0 versions are included in the tutorial with color coded to help the reader to distinguish between them<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/maths/"><font color=#0000FF size=2><B>Maths for Computer Graphics</B></font></A><BR>
+Several maths techniques are described in here. the main idea is to provide a collection of usefull routines for programmers.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/billboarding/"><font color=#0000FF size=2><B>Billboarding tutorial</B></font></A><BR>
+Billboarding is a technique that adjusts an object's orientation so that it "faces" some target, usually the camera. In this tutorial several techniques to implement billboarding are covered. From cheating billboards to true billboards. In all techniques both Cylindrical and Spherical versions are presented.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/picking/"><font color=#0000FF size=2><B>Picking tutorial</B></font></A><BR>
+The OpenGL API provides a mechanism for picking objects in a 3D scene. The Red Book proposes an alternative mechanism based on color coding. This tutorial will show you how to detect which objects are below the mouse or in a square region of the OpenGL window using both approaches. <P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/terrain/"><font color=#0000FF size=2><B>Terrain tutorial</B></font></A><BR>
+Several algorithms to generate artificial terrains, including the Mid Point Displacement algorithm, are presented.
+This tutorial also covers the creation of a height map from an image, and its rendering. Normal computation and lighting simulation for both directional and positional lights is described in detail. A library to load TGA uncompressed images is also provided.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/displaylists/"><font color=#0000FF size=2><B>Display Lists tutorial</B></font></A><BR>
+A tutorial with source code exploring some of the possibilities provided by Display Lists. Enhance your application making it run faster with this simple feature.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/glut/"><font color=#0000FF size=2><B>GLUT tutorial</B></font></A><BR>
+GLUT is a library that allows us to write system independent applications without messing up with specific window systems. In this tutorial I cover the initializations required, mouse and keyboard input, pop-up menus, writing text, GLUT's gamemode, and subwindows.<P>
+</TD>
+</TR>
+<TR>
+<TD>
+</TD>
+<TD><font size="2"><font face="Arial, Helvetica, sans-serif">
+<a href="http://www.lighthouse3d.com/opengl/thirdparty/Shadow_code.zip"><font color=#0000FF size=2><B>Shadows tutorial</B></font></A><BR>
+
+Tutorial and Demo sent by Dario Villa covering Shadows using the stencil buffer and projecting geometry onto planes. Contains an interactive demo with moving lights and geometry. Looks really cool.<P>
+		 </TD>
+	</TR>
+</TABLE>
+<br>
+
+<a href="http://www.lighthouse3d.com">www.lighthouse3d.com</a>
+
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+</body>
+
+</html>

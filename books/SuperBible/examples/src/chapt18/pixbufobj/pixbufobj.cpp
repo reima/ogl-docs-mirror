8df31c24ae2a97bd47ffb811f66d255f290c6f0a
benj <benj@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-02-24 16:18:29 +0000

    First sample for chapter 18.  It's supposed to demonstrate the performance benefits of using PBOs for pixel pack/unpack.  Unfortunately it turns into a slideshow when enabling the PBO path.  It's functionally complete, but needs some perf debugging.

---
+++ books/SuperBible/examples/src/chapt18/pixbufobj/pixbufobj.cpp
@@ -0,0 +1,401 @@
+// pixbufobj.cpp
+// OpenGL SuperBible, Chapter 18
+// Demonstrates use of Pixel Buffer Objects (PBOs)
+// Program by Benjamin Lipchak
+
+#include "../../shared/gltools.h"   // OpenGL toolkit
+#include "../../shared/glFrame.h"   // OpenGL toolkit
+#include "../../shared/stopwatch.h" // General purpose stop watch class
+
+#include <assert.h>
+#include <stdio.h>
+
+GLint windowWidth = 512;            // window size
+GLint windowHeight = 512;
+
+GLint mainMenu;                     // menu handle
+
+GLboolean usePBOs = GL_FALSE;
+GLboolean useMotionBlur = GL_TRUE;
+
+GLubyte* pixels[3];                 // 3 old frames the size of window
+GLuint currentFrame = 0;            // which old frame are we on?
+
+GLfloat angleIncrement = 1.0f;
+
+// Called to draw scene
+void RenderScene(void)
+{
+    static int iFrames = 0;           // Frame count
+    static CStopWatch frameTimer;     // Render time
+ 
+    // Reset the stopwatch on first time
+    if(iFrames == 0)
+    {
+        frameTimer.Reset();
+        iFrames++;
+    }
+
+    int lastFrame = (currentFrame + 2) % 3;
+    int frameBeforeThat = (currentFrame + 1) % 3;
+
+    // Track camera angle
+    glViewport(0, 0, windowWidth, windowHeight);
+    
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT);
+   
+    // Rotate the texture matrix for unit 0 (current frame)
+    glActiveTexture(GL_TEXTURE0);
+    glTranslatef(0.5f, 0.5f, 0.0f);
+    glRotatef(angleIncrement, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-0.5f, -0.5f, 0.0f);
+
+    if (!useMotionBlur)
+    {
+        GLfloat copiedMatrix[16];
+        glGetFloatv(GL_TEXTURE_MATRIX, copiedMatrix);
+        glActiveTexture(GL_TEXTURE1);
+        glLoadMatrixf(copiedMatrix);
+        glBindTexture(GL_TEXTURE_2D, 1);
+    }
+
+    // Draw objects in the scene
+    int i;
+    glBegin(GL_QUADS);
+        for (i = 0; i < 3; i++)
+            glMultiTexCoord2f(GL_TEXTURE0 + i, 0.0f, 0.0f);
+        glVertex2f(-1.0f, -1.0f);
+        for (i = 0; i < 3; i++)
+            glMultiTexCoord2f(GL_TEXTURE0 + i, 0.0f, 1.0f);
+        glVertex2f(-1.0f, 1.0f);
+        for (i = 0; i < 3; i++)
+            glMultiTexCoord2f(GL_TEXTURE0 + i, 1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+        for (i = 0; i < 3; i++)
+            glMultiTexCoord2f(GL_TEXTURE0 + i, 1.0f, 0.0f);
+        glVertex2f(1.0f, -1.0f);
+    glEnd();
+
+    // Now read back result
+    if (usePBOs)
+    {
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, currentFrame + 1);
+        glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+    }
+    else
+    {
+        glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, pixels[currentFrame]);
+    }
+
+    // Prepare the last frame by dividing colors by 4
+    if (usePBOs)
+    {
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, lastFrame + 1);
+        pixels[lastFrame] = (GLubyte*)glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_READ_WRITE);
+    }
+    for (int y = 0; y < windowHeight; y++)
+    {
+        for (int x = 0; x < windowWidth; x++)
+        {
+            GLubyte *ptr = (GLubyte *)pixels[lastFrame] + ((y*windowWidth)+x)*3;
+            *(ptr + 0) >>= 2;
+            *(ptr + 1) >>= 2;
+            *(ptr + 2) >>= 2;
+        }
+    }
+    if (usePBOs)
+    {
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+        pixels[lastFrame] = NULL;
+    }
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, 2+lastFrame);
+    if (usePBOs)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, windowWidth, windowHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    }
+    else
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, windowWidth, windowHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels[lastFrame]);
+    }
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, 2+frameBeforeThat);
+
+    // Advance old frame
+    currentFrame = (currentFrame + 1) % 3;
+
+    if (glGetError() != GL_NO_ERROR)
+        fprintf(stderr, "GL Error!\n");
+
+    // Flush drawing commands
+    glutSwapBuffers();
+
+    // Increment the frame count
+    iFrames++;
+
+    // Do periodic frame rate calculation
+    if (iFrames == 101)
+    {
+        float fps;
+        char cBuffer[64];
+
+        fps = 100.0f / frameTimer.GetElapsedSeconds();
+        if (usePBOs)
+            sprintf(cBuffer,"Draw scene with PBOs %.1f fps", fps);
+        else
+            sprintf(cBuffer,"Draw scene without PBOs %.1f fps", fps);
+
+        glutSetWindowTitle(cBuffer);
+        frameTimer.Reset();
+        iFrames = 1;
+    }
+}
+
+// This function does any needed initialization on the rendering
+// context. 
+void SetupRC()
+{
+    fprintf(stdout, "Pixel Buffer Object Performance Demo\n\n");
+
+    // Make sure required functionality is available!
+    if (!GLEE_VERSION_2_1 && !GLEE_ARB_pixel_buffer_object)
+    {
+        fprintf(stderr, "PBO extension is not available!\n");
+        Sleep(2000);
+        exit(0);
+    }
+
+    fprintf(stdout, "Controls:\n");
+    fprintf(stdout, "\tRight-click for menu\n\n");
+    fprintf(stdout, "\tb\tToggle motion blur\n\n");
+    fprintf(stdout, "\tarrows\t+/- rotation speed\n\n");
+    fprintf(stdout, "\tq\t\tExit demo\n\n");
+    
+    // Colors
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
+    glColor4f(0.0f, 0.0f, 0.0f, 1.0f );
+
+    // Texture matrix is the only one we'll change
+    glMatrixMode(GL_TEXTURE);
+
+    // Create 4 texture objects and load up original texture in all.
+    // Divide colors by 2 and 4 to accommodate our additive blending
+    GLint w, h, c;
+    GLenum format;
+    GLbyte *texels = gltLoadTGA("reservoir.tga", &w, &h, &c, &format);
+    assert(c == GL_RGB8);
+    for (int y = 0; y < h; y++)
+    {
+        for (int x = 0; x < w; x++)
+        {
+            GLubyte *ptr = (GLubyte *)texels + ((y*w)+x)*3;
+            *(ptr + 0) >>= 1;
+            *(ptr + 1) >>= 1;
+            *(ptr + 2) >>= 1;
+        }
+    }
+    // This texture object will hold the current frame, rendered at 50% brightness
+    glBindTexture(GL_TEXTURE_2D, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, c, w, h, 0, format, GL_UNSIGNED_BYTE, texels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    for (int y = 0; y < h; y++)
+    {
+        for (int x = 0; x < w; x++)
+        {
+            GLubyte *ptr = (GLubyte *)texels + ((y*w)+x)*3;
+            *(ptr + 0) >>= 1;
+            *(ptr + 1) >>= 1;
+            *(ptr + 2) >>= 1;
+        }
+    }
+    // These texture objects will hold old frames, rendered at 25% brightness
+    for (int i = 2; i <= 4; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, i);
+        glTexImage2D(GL_TEXTURE_2D, 0, c, w, h, 0, format, GL_UNSIGNED_BYTE, texels);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    }
+    free(texels);
+
+    // Set up texture units for additive 2D multitexturing
+    for (int i = 0; i < 3; i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        glBindTexture(GL_TEXTURE_2D, i+1);
+        glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ADD);
+        glEnable(GL_TEXTURE_2D);
+    }
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void ProcessMenu(int value)
+{
+    switch(value)
+    {
+    case 1:
+        usePBOs = !usePBOs;
+        if (usePBOs)
+        {
+            glutChangeToMenuEntry(1, "Toggle PBO usage (currently ON)", 1);
+
+            // first upload client memory to PBOs, then free them
+            for (int i = 0; i < 3; i++)
+            {
+                glBindBuffer(GL_PIXEL_PACK_BUFFER, i+1);
+                glBufferData(GL_PIXEL_PACK_BUFFER, windowWidth * windowHeight * 3, pixels[i], GL_STREAM_COPY);
+
+                if (pixels[i])
+                {
+                    free(pixels[i]);
+                    pixels[i] = NULL;
+                }
+            }
+            glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+        }
+        else
+        {
+            glutChangeToMenuEntry(1, "Toggle PBO usage (currently OFF)", 1);
+
+            // allocate read buffer, size of window
+            for (int i = 0; i < 3; i++)
+            {
+                if (pixels[i])
+                {
+                    free(pixels[i]);
+                }
+                pixels[i] = (GLubyte*)malloc(windowWidth * windowHeight * 3);
+                assert(pixels[i]);
+            }
+        }
+        break;
+
+    case 2:
+        useMotionBlur = !useMotionBlur;
+        if (useMotionBlur)
+        {
+            glutChangeToMenuEntry(2, "Toggle motion blur (currently ON)", 2);
+            glActiveTexture(GL_TEXTURE1);
+            glLoadIdentity();
+            glActiveTexture(GL_TEXTURE2);
+            glEnable(GL_TEXTURE_2D);
+        }
+        else
+        {
+            glutChangeToMenuEntry(2, "Toggle motion blur (currently OFF)", 2);
+            glActiveTexture(GL_TEXTURE2);
+            glDisable(GL_TEXTURE_2D);
+        }
+        break;
+
+    default:
+        break;
+    }
+
+    // Refresh the Window
+    glutPostRedisplay();
+}
+
+void KeyPressFunc(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'b':
+    case 'B':
+        useMotionBlur = !useMotionBlur;
+        if (useMotionBlur)
+        {
+            glutChangeToMenuEntry(2, "Toggle motion blur (currently ON)", 2);
+            glActiveTexture(GL_TEXTURE1);
+            glLoadIdentity();
+            glActiveTexture(GL_TEXTURE2);
+            glEnable(GL_TEXTURE_2D);
+        }
+        else
+        {
+            glutChangeToMenuEntry(2, "Toggle motion blur (currently OFF)", 2);
+            glActiveTexture(GL_TEXTURE2);
+            glDisable(GL_TEXTURE_2D);
+        }
+        break;
+    case 'q':
+    case 'Q':
+    case 27 : /* ESC */
+        exit(0);
+    }
+
+    // Refresh the Window
+    glutPostRedisplay();
+}
+
+void SpecialKeys(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_UP:
+    case GLUT_KEY_RIGHT:
+        angleIncrement += 1.0f;
+        break;
+    case GLUT_KEY_DOWN:
+    case GLUT_KEY_LEFT:
+        angleIncrement -= 1.0f;
+        break;
+    default:
+        break;
+    }
+
+    // Refresh the Window
+    glutPostRedisplay();
+}
+
+void ChangeSize(int w, int h)
+{
+    windowWidth = w;
+    windowHeight = h;
+
+    // allocate read buffer, size of window
+    for (int i = 0; i < 3; i++)
+    {
+        if (pixels[i])
+        {
+            free(pixels[i]);
+        }
+        pixels[i] = (GLubyte*)malloc(w * h * 3);
+        assert(pixels[i]);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutCreateWindow("Pixel Buffer Object Performance Demo");
+    glutReshapeFunc(ChangeSize);
+    glutKeyboardFunc(KeyPressFunc);
+    glutSpecialFunc(SpecialKeys);
+    glutDisplayFunc(RenderScene);
+    glutIdleFunc(RenderScene);
+
+    SetupRC();
+
+    // Create the menus
+    mainMenu = glutCreateMenu(ProcessMenu);
+    glutAddMenuEntry("Toggle PBO usage (currently OFF)", 1);
+    glutAddMenuEntry("Toggle motion blur (currently ON)", 2);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+    glutMainLoop();
+
+    for (int i = 0; i < 3; i++)
+    {
+        free(pixels[i]);
+    }
+
+    return 0;
+}

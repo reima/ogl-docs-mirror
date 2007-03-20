54cd3092016f2551ff72c6d3f8749f544e5b6c27
benj <benj@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-03-20 04:22:37 +0000

    HDR bloom sample is almost entirely working.  Afterglow just needs a little more work.

--- ogl/trunk/ecosystem/public/sdk/docs/books/SuperBible/examples/src/chapt18/hdrbloom/shaders/combine.fs
+++ ogl/trunk/ecosystem/public/sdk/docs/books/SuperBible/examples/src/chapt18/hdrbloom/shaders/combine.fs
@@ -4,11 +4,14 @@
 // add them together, then
 // tonemap the result
 
+uniform bool afterGlow;
+
 uniform sampler2D sampler0;
 uniform sampler2D sampler1;
 uniform sampler2D sampler2;
 uniform sampler2D sampler3;
 uniform sampler2D sampler4;
+uniform sampler2D sampler5;
 
 void main(void)
 {
@@ -19,6 +22,12 @@ void main(void)
     temp += texture2D(sampler2, gl_TexCoord[0].st);
     temp += texture2D(sampler3, gl_TexCoord[0].st);
     temp += texture2D(sampler4, gl_TexCoord[0].st);
+
+    if (afterGlow)
+    {
+        temp *= 0.6;
+        temp += 0.4 * texture2D(sampler5, gl_TexCoord[0].st);
+    }
 
     gl_FragColor = temp;
 }

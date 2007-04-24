608df8f0f40e3030548e8102baa3fe0cf4d57f88
richard_wright <richard_wright@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-04-24 04:30:19 +0000

    Path problems to shaders on Mac.

--- ogl/trunk/ecosystem/public/sdk/docs/books/SuperBible/examples/src/chapt18/hdrbloom/hdrbloom.cpp
+++ ogl/trunk/ecosystem/public/sdk/docs/books/SuperBible/examples/src/chapt18/hdrbloom/hdrbloom.cpp
@@ -107,7 +107,7 @@ void PrepareShader(GLint shaderNum)
     GLint success;
 
     // Create shader objects and specify shader text
-    sprintf(fullFileName, ".\\shaders\\%s.vs", shaderNames[shaderNum]);
+    sprintf(fullFileName, "./shaders/%s.vs", shaderNames[shaderNum]);
     shString = LoadShaderText(fullFileName);
     if (!shString)
     {
@@ -133,7 +133,7 @@ void PrepareShader(GLint shaderNum)
         exit(0);
     }
 
-    sprintf(fullFileName, ".\\shaders\\%s.fs", shaderNames[shaderNum]);
+    sprintf(fullFileName, "./shaders/%s.fs", shaderNames[shaderNum]);
     shString = LoadShaderText(fullFileName);
     if (!shString)
     {

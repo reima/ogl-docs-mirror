9101d04628c6dfa1f21cde9dcd739e41400cc8a1
benj <benj@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-05-09 19:47:42 +0000

    Add placeholder page for OpenGL SuperBible home in the SDK.

---
+++ books/SuperBible/index.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD>
+<TITLE>OpenGL SDK</TITLE>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
+</HEAD>
+<BODY>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
+<H1>OpenGL SuperBible</H1>
+
+<P>This is the future home of the OpenGL SuperBible.</P>
+
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+</BODY>
+</HTML>

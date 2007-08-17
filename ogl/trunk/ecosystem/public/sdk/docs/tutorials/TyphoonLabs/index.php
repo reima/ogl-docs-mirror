f95376c7c40cf86d4b90266fd1e53ead9bda7ebe
ffelagund <ffelagund@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-08-17 10:08:46 +0000

    Tutorials upload

--- ogl/trunk/ecosystem/public/sdk/docs/tutorials/TyphoonLabs/index.php
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/TyphoonLabs/index.php
@@ -2,10 +2,320 @@
 <HEAD>
 <TITLE>TyphoonLabs Hello World</TITLE>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
-</HEAD>
+<style type="text/css">
+<!--
+.Estilo1 {color: #009999}
+a:link {
+	color: #00CC99;
+}
+-->
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></HEAD>
 <BODY>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
-<H1>TyphoonLabs Hello World!</H1>
-<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+<h2 class="Estilo1"><img src="header.jpg" width="900" height="115"></h2>
+<table width="899" border="o">
+  <tr>
+    <td width="731"><h1 align="center"><span class="Estilo1">TyphoonLabs' OpenGL Shading Language tutorials</span></h1></td>
+    <td width="158"><div align="center"><img src="glsl.jpg" width="100" height="86"></div></td>
+  </tr>
+</table>
+<br>
+<table border="0" cellspacing="0" cellpadding="0" align="left" width="432">
+  <tr>
+    <td colspan="3"><span class="Estilo1"><a href="Chapter_1.pdf">Chapter 1: Introduction to GLSL</a></span></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>An Introduction to Programmable Hardware</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Brief History of the OpenGL Programmable Hardware    Pipeline</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Fixed Function vs. Programmable Function</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56" rowspan="2"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Programmable Function Scheme</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Fixed Function Scheme</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Shader 'Input' Data</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56" rowspan="2"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Uniform Variables</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Vertex Attributes</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Varying Variables</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Shader 'Output' Data</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56" rowspan="2"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Vertex Shader</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Fragment Shader</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Simple GLSL Shader Example</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="3"><p><strong>Shader Designer IDE</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>User Interface</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Toolbar</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Menu</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>State Properties</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="59"><p>&nbsp;<strong> </strong></p></td>
+    <td width="317"><p><strong>Light States</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="59"><p>&nbsp;<strong> </strong></p></td>
+    <td width="317"><p><strong>Vertex States</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="59"><p>&nbsp;<strong> </strong></p></td>
+    <td width="317"><p><strong>Fragment States</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Code Window</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td colspan="2"><p><strong>Uniform Variables Manager</strong></p></td>
+  </tr>
+</table>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<table border="0" cellspacing="0" cellpadding="0" align="left" width="431">
+  <tr>
+    <td colspan="2"><span class="Estilo1"><a href="Chapter_2.pdf">Chapter 2: GLSL Basics</a></span></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Introduction</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>GLSL Language</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56" rowspan="2"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Mechanisms Borrowed from C++</strong></p></td>
+  </tr>
+  <tr>
+    <td width="375"><p><strong>Character Set</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Pre-processor Keywords</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Comments</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Variables and Types</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Type Descriptions</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Type Qualifiers</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Operators and Preference</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Subroutines</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Flow Control</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Built-in Variables</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Built-in Constants</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Built-in Attributes</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>General Built-in Uniform States</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Varying Variables</strong></p></td>
+  </tr>
+  <tr>
+    <td width="56"><p>&nbsp;<strong> </strong></p></td>
+    <td width="375"><p><strong>Built-in Functions</strong></p></td>
+  </tr>
+</table>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<br>
+<table border="0" cellspacing="0" cellpadding="0" align="left" width="428">
+  <tr>
+    <td colspan="2"><a href="Chapter_3.pdf"><span class="Estilo1">Chapter 3: GLSL Basic Shaders</span></a></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Introduction</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>First Basic Shader</strong></p></td>
+  </tr>
+  <tr>
+    <td width="64"><p>&nbsp;<strong> </strong></p></td>
+    <td width="364"><p><strong>Simple Shader 1</strong></p></td>
+  </tr>
+  <tr>
+    <td width="64"><p>&nbsp;<strong> </strong></p></td>
+    <td width="364"><p><strong>Simple Shader 2: Using Varying Variables</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Using Built-in Uniforms and Attributes</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Texturing and Multitexturing (Vertex Attributes)</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Discard Shader and Subroutines</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Simple Illumination Model 1</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Simple Illumination Model 2: Per Vertex Specular    &amp; Glossines</strong></p></td>
+  </tr>
+</table>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<table border="0" cellspacing="0" cellpadding="0" align="left" width="428">
+  <tr>
+    <td><p class="Estilo1"><a href="Chapter_4.pdf">Chapter 4: Advanced Shaders</a></p>    </td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Introduction</strong></p></td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Per Pixel Illumination Model</strong></p></td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Bump Mapping</strong></p></td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Simple Parallax Shader</strong></p></td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Mandelbrot Shader (Flow Control)</strong></p></td>
+  </tr>
+  <tr>
+    <td width="428"><p><strong>Advanced Illumination Model</strong></p></td>
+  </tr>
+</table>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1"><br>
+  <br>
+</p>
+<table border="0" cellspacing="0" cellpadding="0" align="left" width="428">
+  <tr>
+    <td colspan="2"><a href="Chapter_5.pdf"><span class="Estilo1">Chapter 5: Appendix</span></a></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>Using GLSL Shaders Within OpenGL Applications</strong></p></td>
+  </tr>
+  <tr>
+    <td width="47"><p>&nbsp;<strong> </strong></p></td>
+    <td width="381"><p><strong>Loading and Using Shaders</strong></p></td>
+  </tr>
+  <tr>
+    <td width="47"><p>&nbsp;<strong> </strong></p></td>
+    <td width="381"><p><strong>Using Uniforms</strong></p></td>
+  </tr>
+  <tr>
+    <td width="47"><p>&nbsp;<strong> </strong></p></td>
+    <td width="381"><p><strong>Using Textures</strong></p></td>
+  </tr>
+  <tr>
+    <td width="47"><p>&nbsp;<strong> </strong></p></td>
+    <td width="381"><p><strong>Using Vertex Attributes</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>How To Use the 3DSMax5 Mesh Exporter</strong></p></td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><strong>References</strong></p></td>
+  </tr>
+</table>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1">&nbsp;</p>
+<p class="Estilo1"><br>
+Contact: jacobo.rodriguez 'at' typhoonlabs.com or ffelagund 'at' gmail.com</p>
+<p class="Estilo1">
+  <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+</p>
 </BODY>
 </HTML>

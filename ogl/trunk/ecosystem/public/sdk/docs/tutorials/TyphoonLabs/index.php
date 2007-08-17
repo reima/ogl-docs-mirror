c739388e10497009c6745414f5756dd20e10c9cd
ffelagund <ffelagund@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-08-17 09:34:40 +0000

    First svn test

---
+++ ogl/trunk/ecosystem/public/sdk/docs/tutorials/TyphoonLabs/index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+<TITLE>TyphoonLabs Hello World</TITLE>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
+</HEAD>
+<BODY>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
+<H1>TyphoonLabs Hello World!</H1>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+</BODY>
+</HTML>

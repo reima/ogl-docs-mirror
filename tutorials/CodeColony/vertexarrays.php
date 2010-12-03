c1c31023da07d7d4a762e9a5a161e76cf75e5d07
root <root@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2010-12-03 00:55:14 +0000

    More PHP include fixes.

--- tutorials/CodeColony/vertexarrays.php
+++ tutorials/CodeColony/vertexarrays.php
@@ -1,11 +1,11 @@
 <html>
 <head>
 <title>Vertex Arrays - by CodeColony.de</title>
-<?php include("/home/virtual/opengl.org/var/www/html/inc/sdk_head.txt"); ?>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
 </head>
 
 <body>
-<?php include("/home/virtual/opengl.org/var/www/html/inc/sdk_body_start.txt"); ?>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
 <h1>Vertex Arrays</h1>
 <h2>Before you start</h2>
 <p>

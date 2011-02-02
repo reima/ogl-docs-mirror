9937b163eb4facdd3d49a9469040867d5e72f297
groovounet <groovounet@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2011-02-02 14:10:48 +0000

    Check-in index template

---
+++ tutorials/OGLSamples/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>OpenGL Samples Pack SDK contribution</title>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
+</head>
+
+<body>
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
+<h1>OpenGL Samples Pack</h1>
+
+<?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
+</body>
+
+</html>

0130373b2784654365d2d6ba6a2ada9950a9a9cb
gsellers <gsellers@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2013-08-06 16:26:42 +0000

    Update OpenGL SuperBible page to talk about 6th edition.

--- books/SuperBible/index.php
+++ books/SuperBible/index.php
@@ -7,54 +7,55 @@
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
 <H1>OpenGL SuperBible</H1>
 <H2>Comprehensive Tutorial and Reference</H2>
-<H4>by Richard S. Wright, Nicholas Haemel, Graham Sellers and Benjamin Lipchak</H4>
+<H4>by Graham Sellers, Richard S. Wright and Nicholas Haemel</H4>
 
 <TABLE CELLSPACING="20"><TR>
 <TD><IMG SRC="images/cover1.gif" ALT="First Edition" WIDTH="145" HEIGHT="186"></TD>
 <TD><IMG SRC="images/cover2.gif" ALT="Second Edition" WIDTH="150" HEIGHT="186"></TD>
 <TD><IMG SRC="images/cover3.jpg" ALT="Third Edition" WIDTH="150" HEIGHT="186"></TD>
 <TD><IMG SRC="images/cover4.jpg" ALT="Fourth Edition" WIDTH="151" HEIGHT="186"></TD>
-<TD><A HREF="http://www.amazon.com/exec/obidos/ASIN/0321712617/khongrou-20"><IMG SRC="images/cover5.jpg" ALT="Fifth Edition" WIDTH="151" HEIGHT="186"></A></TD>
+<TD><IMG SRC="images/cover5.jpg" ALT="Fifth Edition" WIDTH="151" HEIGHT="186"></TD>
+<TD><A HREF="http://www.amazon.com/exec/obidos/ASIN/0321902947/khongrou-20"><IMG SRC="images/cover6.png" ALT="Sixth Edition" WIDTH="151" HEIGHT="186"></A></TD>
 </TR></TABLE>
 
-<P>The fifth edition of OpenGL&reg; SuperBible, the newest member of the Addison Wesley 
+<P>The sixth edition of OpenGL&reg; SuperBible, the newest member of the Addison Wesley
 OpenGL Technical Library, is now available!</P>
 
-<P>OpenGL SuperBible, Fifth Edition is the definitive programmer’s guide, tutorial, and
-reference for the world’s leading 3D API for real-time computer graphics, OpenGL 3.3.
+<P>
+OpenGL® SuperBible, Sixth Edition, is the definitive programmer’s guide, tutorial, and
+reference for the world’s leading 3D API for real-time computer graphics, OpenGL 4.3.
 The best all-around introduction to OpenGL for developers at all levels of experience,
-it clearly explains both the API and essential associated programming concepts. Readers
-will find up-to-date, hands-on guidance on all facets of modern OpenGL development,
-including transformations, texture mapping, shaders, advanced buffers, geometry management,
-and much more. Fully revised to reflect ARB’s official OpenGL 3.3 specification, this
-edition also contains a new start-to-finish tutorial on OpenGL for the iPhone, iPod touch, and iPad.</P>
+it clearly explains both the newest API and indispensable related concepts. You’ll find
+up-to-date, hands-on guidance for all facets of modern OpenGL development on both
+desktop and mobile platforms, including transformations, texture mapping, shaders, buffers,
+geometry management, and much more.
+</P>
 
-<P>Coverage includes:
+<P>
+Extensively revised, this edition presents many new OpenGL 4.3 features, including
+compute shaders, texture views, indirect draws, and enhanced API debugging. It has
+been reorganized to focus more tightly on the API, to cover the entire pipeline
+earlier, and to help you thoroughly understand the interactions between OpenGL and
+graphics hardware.
+</P>
+
+<H2>Coverage includes</H2>
+
 <UL>
-
-
-<LI>A practical introduction to the essentials of real-time 3D graphics</LI>
-<LI>Core OpenGL 3.X techniques for rendering, transformations, and texturing</LI>
-<LI>Using both stock and custom shaders—and starting to write your own</LI>
-<LI>Cross-platform OpenGL: Windows (including Windows 7), Mac OS X, GNU/Linux, UNIX, and embedded systems</LI>
-<LI>OpenGL programming for iPhone, iPod touch, and iPad: step-by-step guidance and complete example programs</LI>
-<LI>Advanced buffer techniques, including full-definition rendering with floating point buffers and textures</LI>
-<LI>Fragment operations: controlling the end of the graphics pipeline</LI>
-<LI>Advanced shader usage and geometry management</LI>
-<LI>A fully updated API reference, now based on the official ARB (Core) OpenGL 3.3 manual pages</LI>
-<LI>New bonus materials and sample code on a companion Web site</LI>
+<LI>A practical introduction to the essentials of realtime 3D graphics</LI>
+<LI>Core OpenGL 4.3 techniques for rendering, transformations, and texturing</LI>
+<LI>Foundational math for creating interesting 3D graphics with OpenGL</LI>
+<LI>Writing your own shaders, with examples to get you started</LI>
+<LI>Cross-platform OpenGL, including essential platform-specific API initialization material for Linux, OS X, and Windows</LI>
+<LI>Vertex processing, drawing commands, primitive processing, fragments, and framebuffers</LI>
+<LI>Using compute shaders to harness today’s graphics cards for more than graphics</LI>
+<LI>Monitoring and controlling the OpenGL graphics pipeline</LI>
+<LI>Advanced rendering: light simulation, artistic and non-photo-realistic rendering, and deferred shading</LI>
+<LI>Modern OpenGL debugging and performance optimization</LI>
 </UL>
 
-<H2>Sample Code From Fourth Edition</H2>
-
-<TABLE>
-<TR><TD>Complete source code for all platforms (182 MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-AllSource.zip">SB-AllSource.zip</A></TD></TR>
-<TR><TD>Source with pre-built binaries (Windows, 120MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-WinwBin.zip">SB-WinwBin.zip</A></TD></TR>
-<TR><TD>Source with pre-built binaries (Mac OS X, 208MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-MacwBin.zip">SB-MacwBin.zip</A></TD></TR>
-<TR><TD>Source only, no binaries (Windows, 60MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-WinSrc.zip">SB-WinSrc.zip</A></TD></TR>
-<TR><TD>Source only, no binaries (Mac OS X, 71MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-MacSrc.zip">SB-MacSrc.zip</A></TD></TR>
-<TR><TD>Source only, no binaries (Linux, 48MB):</TD><TD><A HREF="http://www.starstonesoftware.com/files/SB-LinuxSrc.tar.bz2">SB-LinuxSrc.tar.bz2</A></TD></TR>
-</TABLE>
+The book's website is at <a href="http://www.openglsuperbible.com/">http://www.openglsuperbible.com/</a> and includes
+sample code and more.
 
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
 </BODY>

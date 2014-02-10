c6297aa69d3a45adf4004c43b7c4a352a6661f4a
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2014-02-10 10:44:06 +0000

    Move GLSL ref pages to Docbook 5 source / HTML5 output, merge with API ref pages in ../man4, cache old Docbook 4 / XHTML Transitional pages under docbook4/, and add redirection links for people arriving from search engines.

--- manglsl/index.php
+++ manglsl/index.php
@@ -1,9 +1,10 @@
-<HTML>
-<HEAD>
-<TITLE>OpenGL Shading Language (GLSL) Reference Pages</TITLE>
-</HEAD>
-<FRAMESET ROWS="160,*">
-  <FRAME SCROLLING="no" NORESIZE FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SRC="top.php">
-  <FRAME NORESIZE FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SRC="bottom.php">
-</FRAMESET>
-</HTML>
+<html>
+<head>
+<meta http-equiv="REFRESH" content="10; url=new.html">
+</head>
+<body>
+The OpenGL Shading Language reference pages have been merged with the
+OpenGL API reference pages. Please wait to be redirected to the new
+common index page, or click <a href="../man4/index.php">here</a>.
+</body>
+</html>

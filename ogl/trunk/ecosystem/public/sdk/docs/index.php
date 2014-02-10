67dd63b960fe2401384a409102c2786eecb5de52
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2014-02-10 10:48:30 +0000

    A few more fixes.

--- ogl/trunk/ecosystem/public/sdk/docs/index.php
+++ ogl/trunk/ecosystem/public/sdk/docs/index.php
@@ -1,36 +1,36 @@
-<HTML>
-<HEAD>
-<TITLE>OpenGL SDK</TITLE>
+<html>
+<head>
+<title>OpenGL SDK</title>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
-</HEAD>
-<BODY>
+</head>
+<body>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
-<H1>Documentation</H1>
+<h1>Documentation</h1>
 
-<H2>The OpenGL Reference Pages</H2>
+<h2>The OpenGL Reference Pages</h2>
 
-<P>The <A HREF="./man4/">reference pages for OpenGL 4</A> include
+<p>The <a href="./man4/">reference pages for OpenGL 4</a> include
 complete descriptions of all OpenGL commands contained in the OpenGL 4
 Specification and the functions contained in the OpenGL Shading Language
-4.40 Specification. </P>
+4.40 Specification. </p>
 
-<P>The <A HREF="./man3/">reference pages for OpenGL 3.3</A> include
+<p>The <a href="./man3/">reference pages for OpenGL 3.3</a> include
 complete descriptions of all OpenGL commands contained in the Core
-Profile of the OpenGL 3.3 Specification.</P>
+Profile of the OpenGL 3.3 Specification.</p>
 
-The legacy <A HREF="./man2/">OpenGL 2.1 reference pages</A> include
+<p> The legacy <a href="./man2/">OpenGL 2.1 reference pages</a> include
 descriptions of all OpenGL commands contained in the OpenGL 2.1
 Specification, including commands that have been deprecated and removed
 in more modern versions of OpenGL (but are still included in
 compatibility profiles of OpenGL). Also included are descriptions of GLU
-and GLX entry points.
+and GLX entry points. </p>
 
-<H2>Quick Reference Cards</H2>
+<h2>Quick Reference Cards</h2>
 
-<P>Several quick reference guides are available in PDF format from the
+<p>Several quick reference guides are available in PDF format from the
 links below. The quick reference guides are fold-out annotated lists of
 the functions included in OpenGL, along with their parameters and other
-useful information.</P>
+useful information.</p>
 
 <ul>
 <li> <a href="http://www.opengl.org/sdk/docs/reference_card/opengl44-quick-reference-card.pdf">
@@ -49,27 +49,27 @@ useful information.</P>
      OpenGL 3.2 Reference Card </a></li>
 </ul>
 
-<P> The <A TARGET="_new" HREF="http://www.opengl.org/registry/"> OpenGL
-Registry</A>, hosted outside of the SDK, contains the formal
+<p> The <a target="_new" href="http://www.opengl.org/registry/"> OpenGL
+Registry</a>, hosted outside of the SDK, contains the formal
 Specifications for OpenGL and the OpenGL Shading Language, as well as
 for companion APIs like GLX and GLU. Specifications are the
 authoritative documents about how OpenGL is intended to work. They are
 also the most difficult to read, being written primarily for OpenGL
 implementers, not developers using OpenGL; so you may find the reference
-pages combined with tutorials to be a better choice. </P>
+pages combined with tutorials to be a better choice. </p>
 
-<P> The Registry also contains specifications for extensions describing
+<p> The Registry also contains specifications for extensions describing
 optional features of OpenGL, and header files defining interfaces for
 extensions. Extension specifications are written as supplements to the
 core OpenGL Specification. Most new features are first introduced as
 extensions and only later become part of a core OpenGL version, so using
 extensions is important to staying current with the evolution of the
-API. </P>
+API. </p>
 
-<P> Please use the <A
-HREF="http://www.opengl.org/documentation/specs/feedback/">feedback
-form</A> to let us know about any issues you find. </P>
+<p> Please use the <a
+href="http://www.opengl.org/documentation/specs/feedback/">feedback
+form</a> to let us know about any issues you find. </p>
 
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
-</BODY>
-</HTML>
+</body>
+</html>

8d1cdf65c31c3034bc3385ccabf187975c85c364
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2014-02-11 10:08:54 +0000

    Fix link.

--- man4/top.php
+++ man4/top.php
@@ -1,10 +1,10 @@
-<HTML>
-<HEAD>
-<TITLE>OpenGL SDK</TITLE>
+<html>
+<head>
+<title>OpenGL SDK</title>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_head.txt"); ?>
-</HEAD>
-<BODY>
+</head>
+<body>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_body_start.txt"); ?>
 <?php include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"); ?>
-</BODY>
-</HTML>
+</body>
+</html>

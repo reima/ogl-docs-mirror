755e34cf60f42d1973b285c3c9230e89ea88174a
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2014-08-22 20:11:15 +0000

    Fix links to nonexistent feedback form.

--- index.php
+++ index.php
@@ -9,28 +9,29 @@
 
 <h2>The OpenGL Reference Pages</h2>
 
-<p>The <a href="./man4/">reference pages for OpenGL 4</a> include
-complete descriptions of all OpenGL commands contained in the OpenGL 4
-Specification and the functions contained in the OpenGL Shading Language
-4.40 Specification. </p>
+<p> The <a href="./man4/">reference pages for OpenGL 4.5</a> include
+    complete descriptions of all OpenGL commands and functions contained in
+    the Core Profile of OpenGL 4.5, and the OpenGL Shading Language 4.40
+    Specification. </p>
 
-<p>The <a href="./man3/">reference pages for OpenGL 3.3</a> include
-complete descriptions of all OpenGL commands contained in the Core
-Profile of the OpenGL 3.3 Specification.</p>
+<p> The legacy <a href="./man3/">reference pages for OpenGL 3.3</a> include
+    complete descriptions of all commands and functions contained in the
+    Core Profile of OpenGL 3.3, and the OpenGL Shading Language 3.30
+    Specifications. </p>
 
 <p> The legacy <a href="./man2/">OpenGL 2.1 reference pages</a> include
-descriptions of all OpenGL commands contained in the OpenGL 2.1
-Specification, including commands that have been deprecated and removed
-in more modern versions of OpenGL (but are still included in
-compatibility profiles of OpenGL). Also included are descriptions of GLU
-and GLX entry points. </p>
+    descriptions of all OpenGL commands contained in the OpenGL 2.1
+    Specification, including commands that have been deprecated and removed
+    in more modern versions of OpenGL (but are still included in
+    compatibility profiles of OpenGL). Also included are descriptions of GLU
+    and GLX entry points. </p>
 
 <h2>Quick Reference Cards</h2>
 
-<p>Several quick reference guides are available in PDF format from the
-links below. The quick reference guides are fold-out annotated lists of
-the functions included in OpenGL, along with their parameters and other
-useful information.</p>
+<p> Several quick reference guides are available in PDF format from the
+    links below. The quick reference guides are fold-out annotated lists of
+    the functions included in OpenGL, along with their parameters and other
+    useful information.</p>
 
 <ul>
 <li> <a href="http://www.opengl.org/sdk/docs/reference_card/opengl45-reference-card.pdf">
@@ -52,21 +53,21 @@ useful information.</p>
 </ul>
 
 <p> The <a target="_new" href="http://www.opengl.org/registry/"> OpenGL
-Registry</a>, hosted outside of the SDK, contains the formal
-Specifications for OpenGL and the OpenGL Shading Language, as well as
-for companion APIs like GLX and GLU. Specifications are the
-authoritative documents about how OpenGL is intended to work. They are
-also the most difficult to read, being written primarily for OpenGL
-implementers, not developers using OpenGL; so you may find the reference
-pages combined with tutorials to be a better choice. </p>
+    Registry</a>, hosted outside of the SDK, contains the formal
+    Specifications for OpenGL and the OpenGL Shading Language, as well as
+    for companion APIs like GLX and GLU. Specifications are the
+    authoritative documents about how OpenGL is intended to work. They are
+    also the most difficult to read, being written primarily for OpenGL
+    implementers, not developers using OpenGL; so you may find the reference
+    pages combined with tutorials to be a better choice. </p>
 
 <p> The Registry also contains specifications for extensions describing
-optional features of OpenGL, and header files defining interfaces for
-extensions. Extension specifications are written as supplements to the
-core OpenGL Specification. Most new features are first introduced as
-extensions and only later become part of a core OpenGL version, so using
-extensions is important to staying current with the evolution of the
-API. </p>
+    optional features of OpenGL, and header files defining interfaces for
+    extensions. Extension specifications are written as supplements to the
+    core OpenGL Specification. Most new features are first introduced as
+    extensions and only later become part of a core OpenGL version, so using
+    extensions is important to staying current with the evolution of the
+    API. </p>
 
 <p>Please use the <a href="https://www.khronos.org/bugzilla/enter_bug.cgi?product=OpenGL">public bugtracker</a> to let us know about any issues you find.</p>
 
